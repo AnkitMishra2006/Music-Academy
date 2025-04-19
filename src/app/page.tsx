c9ddd3ade130eb675c333bc4840e12e3dd94946a
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-04-19 23:05:06 +0530

    Testimonial Cards Section Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <HeroSection></HeroSection>
         <FeaturedCourses></FeaturedCourses>
         <WhyChooseUs></WhyChooseUs>
+        <MusicSchoolTestimonials></MusicSchoolTestimonials>
       </main>
     </>
   );
