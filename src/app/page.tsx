e0e18061a4279938f5677019af401570a2e03b68
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-04-19 22:49:44 +0530

    Sticky Scroll Section Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection></HeroSection>
         <FeaturedCourses></FeaturedCourses>
+        <WhyChooseUs></WhyChooseUs>
       </main>
     </>
   );
